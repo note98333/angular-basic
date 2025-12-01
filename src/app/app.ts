69c96f0b688f94fef2d3896cd75d59627d9ee360
note98333 <touoppo828@gmail.com>
2025-12-01 15:45:56 +0700

    testDarkMode Font Time

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,45 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { NgClass,NgStyle,UpperCasePipe,LowerCasePipe,CurrencyPipe,DatePipe } from '@angular/common';
 
 @Component({
+  standalone: true,
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule,NgClass,NgStyle,UpperCasePipe,LowerCasePipe,CurrencyPipe,DatePipe],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   imageUrl = 'https://www.borrowmydoggy.com/_next/image?url=https%3A%2F%2Fcdn.sanity.io%2Fimages%2F4ij0poqn%2Fproduction%2Fe24bfbd855cda99e303975f2bd2a1bf43079b320-800x600.jpg&w=1080&q=80'
+  size = 200
+  color = "blue"
+  doubleSize(){
+    return this.size * 2;
+  }
+
+  showMessage(){
+    alert("Hello thesst");
+  }
+
+  username = "admin"
+
+  data = [
+    {id:1, name: "thesst",salary: 25000,active: "6/20/2025"},
+    {id:2, name: "kong",salary: 15000,active: "10/8/2025"},
+    {id:3, name: "long",salary: 6000,active: "01/12/2025"},
+  ]
+
+  clearData(){
+    this.data = [];
+  }
+
+  isActive = false;
+  changMode(){
+    this.isActive = !this.isActive
+  }
+  myStyle={
+    'background':'blue',
+    'color':'white',
+    'font-size.px': 20,
+  }
 }
